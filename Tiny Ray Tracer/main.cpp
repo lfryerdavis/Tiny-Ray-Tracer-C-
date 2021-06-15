d31a4d79d801c36e8b26854259caaaa7f7934b76
lfryerdavis <lfryerdavis@gmail.com>
2021-06-15 15:21:09 -0400

    Initial Commit

---
+++ Tiny Ray Tracer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tiny Ray Tracer
+//
+//  Created by Leyland Fryer-Davis on 2021-06-15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
